f5d98ddecfc162f6d2df6e91cf8f83d7db404068
samueleroba <95041024+samueleroba@users.noreply.github.com>
2022-01-17 17:45:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,20 @@ $(document).ready(function(){
 });
 
 
- 
+ // Metodo di test
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+    console.log('User signed out.');
+    window.location.replace("login.html");
+    });
+}
+
+function onLoad() {
+    gapi.load('auth2', function() {
+        gapi.auth2.init();
+    });
+}
 
 
 $(function() {
